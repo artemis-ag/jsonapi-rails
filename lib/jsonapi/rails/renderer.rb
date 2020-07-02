66fee41c35a44cb952e3fc4108ede19041bb4575
Julien Fitzpatrick <jfitzpatrick@artemisag.com>
2020-07-02 08:15:24 -0700

    Move error formatting into its own method

--- lib/jsonapi/rails/renderer.rb
+++ lib/jsonapi/rails/renderer.rb
@@ -50,57 +50,7 @@ module JSONAPI
 
       def render(errors, options, controller)
         options = default_options(options, controller)
-        errors = [errors] unless errors.is_a?(Array)
-
-        # When rendering bulk errors, each item in the errors argument represents
-        # errors for the corresponding resource in the payload.
-        #
-        # Given the payload looks something like:
-        #   {
-        #     data: [
-        #       {
-        #         id: 1,
-        #         attributes: { ... }
-        #       },
-        #       {
-        #         id: 2,
-        #         attributes: { ... }
-        #       },
-        #       {
-        #         id: 3,
-        #         attributes: { ... }
-        #       }
-        #     ]
-        #   }
-        #
-        # then the ErrorsRenderer must be invoked with 3 errors objects
-        #
-        #   render :jsonapi_errors [errors_for_1, errors_for_2, errors_for_3],
-        #          extensions: ['bulk']
-        #
-        # Errors can be any class that is registered in the jsonapi_errors_class
-        # configuration, or an array of any such classes. However, there *must*
-        # be an object to represent errors for each resource from the request.
-        #
-        # If there are no errors for a given object, an empty array will serve
-        # fine to indicate so. Note that an instance of ActiveModel::Errors will
-        # still exist even if there are no current errors for a given model in rails.
-
-        errors = if options[:extensions].include? 'bulk'
-          pointers = [controller.jsonapi_pointers].flatten
-
-          if (errors.length != pointers.length)
-            raise ArgumentError, 'Mismatch between number of resources submitted and errors reported. If there are no errors for a given record please provide an empty array.'
-          end
-
-          errors.zip(pointers).map do |e, reverse_mapping|
-            wrap_errors(e, reverse_mapping)
-          end
-        else
-          errors.map do |e|
-            wrap_errors(e, controller.jsonapi_pointers)
-          end
-        end.flatten.compact
+        errors = format_errors(errors, options, controller)
 
         @renderer.render_errors(errors, options)
       end
@@ -119,6 +69,59 @@ module JSONAPI
                            controller.jsonapi_object
           opts[:extensions] = opts[:extensions] || []
         end
+      end
+
+      # when rendering bulk errors, each item in the errors argument represents
+      # errors for the corresponding resource in the payload.
+      #
+      # if the payload looks like
+      #   {
+      #     data: [
+      #       {
+      #         id: 1,
+      #         attributes: { ... }
+      #       },
+      #       {
+      #         id: 2,
+      #         attributes: { ... }
+      #       },
+      #       {
+      #         id: 3,
+      #         attributes: { ... }
+      #       }
+      #     ]
+      #   }
+      #
+      # then the ErrorsRenderer must be invoked with 3 errors objects
+      #
+      #   render :jsonapi_errors [errors_for_1, errors_for_2, errors_for_3],
+      #          extensions: ['bulk']
+      #
+      # Errors can be any class that is registered in the jsonapi_errors_class
+      # configuration, or an array of any such classes. However, there *must*
+      # be an object to represent errors for each resource from the request.
+      #
+      # If there are no errors for a given object, an empty array will serve
+      # fine to indicate so. Note that an instance of ActiveModel::Errors will
+      # still exist even if there are no current errors for a given model in rails.
+      def format_errors(errors, options, controller)
+        errors = [errors] unless errors.is_a?(Array)
+
+        if options[:extensions].include? 'bulk'
+          pointers = [controller.jsonapi_pointers].flatten
+
+          if (errors.length != pointers.length)
+            raise ArgumentError, 'Mismatch between number of resources submitted and errors reported. If there are no errors for a given record please provide an empty array.'
+          end
+
+          errors.zip(pointers).map do |e, reverse_mapping|
+            wrap_errors(e, reverse_mapping)
+          end
+        else
+          errors.map do |e|
+            wrap_errors(e, controller.jsonapi_pointers)
+          end
+        end.flatten.compact
       end
 
       def wrap_errors(errors, reverse_mapping)
